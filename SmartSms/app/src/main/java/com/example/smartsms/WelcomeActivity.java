4c3d0c83dc1734d197d29c4afa3db1b05ca446d2
KamilBlanik <235629@student.pwr.edu.pl>
2019-04-16 19:44:56 +0200

    Added Welcome Screen

---
+++ SmartSms/app/src/main/java/com/example/smartsms/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.smartsms;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private static int TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+            }
+        },TIME_OUT);
+    }
+}
